a1a3db5d9385d6857a678296b67c300df4d69d47
administrator <testing@test.com>
2023-10-01 00:03:11 -0400

    added --cidr and --list support. fixed extra () in IP list.

--- netmapper.py
+++ netmapper.py
@@ -1,6 +1,7 @@
 import subprocess
 import ipaddress
 import sys
+import argparse
 
 def run_nmap_scan(target):
     try:
@@ -15,28 +16,36 @@ def parse_live_hosts(nmap_output):
     live_hosts = []
     for line in lines:
         if "Nmap scan report for" in line:
-            ip = line.split(" ")[-1]
+            ip = line.split(" ")[-1].strip("()")
             live_hosts.append(ip)
     return live_hosts
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 netmapper.py <CIDR>")
+    parser = argparse.ArgumentParser(description="Network Mapper")
+    parser.add_argument("--cidr", type=str, help="CIDR range for scanning a single subnet")
+    parser.add_argument("--list", type=str, help="Comma-separated list of CIDR ranges for scanning multiple subnets")
+    args = parser.parse_args()
+
+    if args.cidr:
+        targets = [args.cidr]
+    elif args.list:
+        targets = args.list.split(",")
+    else:
+        print("Either --cidr or --list must be provided.")
         sys.exit(1)
 
-    target = sys.argv[1]
+    for target in targets:
+        try:
+            ipaddress.ip_network(target, strict=False)
+        except ValueError:
+            print(f"Invalid CIDR range: {target}")
+            continue
 
-    try:
-        ipaddress.ip_network(target, strict=False)
-    except ValueError:
-        print("Invalid CIDR range.")
-        sys.exit(1)
+        print(f"Scanning target: {target}")
+        nmap_output = run_nmap_scan(target)
+        live_hosts = parse_live_hosts(nmap_output)
 
-    print(f"Scanning target: {target}")
-    nmap_output = run_nmap_scan(target)
-    live_hosts = parse_live_hosts(nmap_output)
-
-    print(f"Live hosts in {target}: {live_hosts}")
+        print(f"Live hosts in {target}: {live_hosts}")
 
 if __name__ == "__main__":
     main()
